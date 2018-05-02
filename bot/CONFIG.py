9afa412df00952f7a15dc4aebab5c339809691bf
LI, Wai Yin <p768lwy3@gmail.com>
2018-05-02 18:22:41 +0800

    Update CONFIG.py

--- bot/CONFIG.py
+++ bot/CONFIG.py
@@ -2,7 +2,7 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s -  %(levelname)s: \n %(message)s', level = logging.INFO)
 
 config = {
-    'TGTOKEN': '424469480:AAGho7jan2s5NKKXHQNDtIrughhjdR0EGr8',
+    'TGTOKEN': '',
     'INPUT': './data/',
     'START': 2017,
     'END': 2017,
